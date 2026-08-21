6d443f0552811d0c9b9f4633be29113bd9ee0359
Monisha <monisha@users.noreply.github.com>
2026-08-21 11:04:21 +0530

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/pet-care-/',
+  base: '/pet-care-frontend/',
   plugins: [react()],
 })
